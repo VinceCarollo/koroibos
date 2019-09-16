466ededf6b53f40cfac907b23429f4e7db7e4793
Vince Carollo <vincecarollo@gmail.com>
2019-09-16 11:57:20 -0600

    Add model tests for olympians, olympian medals, events, and sports

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :sport }
+    it { should have_many :olympian_medals }
+    it { should have_many :olympians }
+  end
+
+end
---
+++ spec/models/olympian_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Olympian, type: :model do
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :team }
+    it { should validate_presence_of :age }
+  end
+
+  describe 'relationships' do
+    it { should have_many :olympian_medals }
+    it { should have_many :events }
+    it { should belong_to :sport }
+  end
+
+end
