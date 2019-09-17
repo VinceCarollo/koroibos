ffdb9916859134196ecac71b21fe86a2702f7677
Vince Carollo <vincecarollo@gmail.com>
2019-09-16 18:32:48 -0600

    Add tests for endpoint to get all olympians

---
+++ spec/requests/api/v1/olympians_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Olympian API" do
+  before :each do
+    @olympian1 = create(:olympian)
+    @olympian2 = create(:olympian)
+  end
+
+  it "gets all olympians" do
+    headers = {
+      content_type: "application/json",
+      accept: "application/json"
+    }
+
+    get '/api/v1/olympians', headers: headers
+
+    expect(response).to be_successful
+    expect(status).to eq(200)
+
+    olympians_data = JSON.parse(response.body, symbolize_names: true)[:olympians]
+
+    expect(olympians_data).to be_a Array
+    expect(olympians_data.count).to eq(2)
+
+    expect(olympians_data.first[:name]).to eq(@olympian1.name)
+    expect(olympians_data.first[:team]).to eq(@olympian1.team)
+    expect(olympians_data.first[:age]).to eq(@olympian1.age)
+    expect(olympians_data.first[:sex]).to eq(@olympian1.sex)
+    expect(olympians_data.first[:total_medals_won]).to eq(@olympian1.total_medals_won)
+  end
+end
